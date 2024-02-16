71f354b84f437f6f6b35c5bbeec12e065753de93
Emiliqna <e.stankova2019@pmg-pernik.com>
2024-02-16 12:02:59 +0200

    Add a page Contacts

--- DechkoWebApp/DechkoWebApp/Controllers/HomeController.cs
+++ DechkoWebApp/DechkoWebApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace DechkoWebApp.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
